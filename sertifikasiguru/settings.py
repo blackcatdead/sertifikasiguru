254b51a23d7c6ec115393ab7883fdd080d7f7cfd
rikhsan <rakhmad.ikhsanudin@gmail.com>
2018-10-13 08:43:17 +0700

    asasd

--- sertifikasiguru/settings.py
+++ sertifikasiguru/settings.py
@@ -24,7 +24,11 @@ SECRET_KEY = 'cs4(0#4#0vdifnefstqvwhle-71!=*mg%o!o)yee68e%8f%!zr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 import sys
-DEBUG = (sys.argv[1] == 'runserver')
+try:
+    DEBUG = (sys.argv[1] == 'runserver')
+except NameError:
+    DEBUG = False
+
 print(DEBUG)
 ALLOWED_HOSTS = ['localhost', 'sg.rikhsan.com', '178.128.110.195']
 
